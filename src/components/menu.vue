7f2d5d1b41bbdb78b016c1f4ad4424689e307621
Major_ <1293963839@qq.com>
2018-02-07 15:18:12 +0800

    【新增】新增mock数据

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -5,7 +5,7 @@
         <span class="count-list" v-if="item.count" >{{item.count}}</span><!--数字-->
         {{item.title}} <!--菜单内容-->
       </a>
-    <a class=" link-list-new" > <!--新增菜单-->
+    <a class=" link-list-new" @click="addTodoList"> <!--新增菜单-->
       <span class="icon-plus">
       </span>
       新增
@@ -13,16 +13,37 @@
   </div>
 </template>
 <script>
-  export default {
-    data() { //data函数
+  import { getTodoList, addTodo } from '../api/api'; // 引入我们 封装好的两个接口函数。
+export default {
+  data() {
     return {
-        items: [{ title: '星期一', count: 1, locked: true }, //菜单的模拟数据
-            { title: '星期二', count: 2, locked: true }, {
-            title: '星期三', count: 3, locked: false
-        }]
-        };
+      items: [], // 菜单数据
+      todoId: '' // 默认选中id
+    };
+  },
+  created() { // 调用请求菜单列表数据的接口
+    getTodoList({}).then(res => {
+      const TODOS = res.data.todos; // 数据都会返回在res.data里面。
+      this.items = TODOS; // 我的把菜单数据赋值给定义的this.items
+      this.todoId = TODOS[0].id; // 把菜单数据的默认的第一个对象的id赋值给默认选中的id
+    });
+  },
+  methods: {
+    goList(id) { // 点击菜单时候,替换选中id
+      this.todoId = id;
+    },
+    addTodoList() { // 点击新增按钮时候
+    // 调用新增菜单的接口，在接口调用成功在请求数据
+      addTodo({}).then(data => {
+        getTodoList({}).then(res => {
+          const TODOS = res.data.todos;
+          this.todoId = TODOS[TODOS.length - 1].id;
+          this.items = TODOS;
+        });
+      });
     }
-  };
+  }
+};
 </script>
 <style lang="less">
 @import '../common/style/menu.less';
